3406fc7df47b8753d24a654ff04240ce35de46a2
K.YOSHIZAWA <yszwken45@gmail.com>
2026-02-07 21:35:32 +0900

    Updateconstants logic

--- initialize.py
+++ initialize.py
@@ -77,28 +77,6 @@ def initialize_session_id():
         st.session_state.session_id = uuid4().hex
 
 
-# def initialize_logger():
-#     """
-#     ログ出力の設定
-#     """
-#     os.makedirs(ct.LOG_DIR_PATH, exist_ok=True)
-
-#     logger = logging.getLogger(ct.LOGGER_NAME)
-
-#     if logger.hasHandlers():
-#         return
-
-#     log_handler = TimedRotatingFileHandler(
-#         os.path.join(ct.LOG_DIR_PATH, ct.LOG_FILE),
-#         when="D",
-#         encoding="utf8"
-#     )
-#     formatter = logging.Formatter(
-#         f"[%(levelname)s] %(asctime)s line %(lineno)s, in %(funcName)s, session_id={st.session_state.session_id}: %(message)s"
-#     )
-#     log_handler.setFormatter(formatter)
-#     logger.setLevel(logging.INFO)
-#     logger.addHandler(log_handler)
 
 
 def initialize_logger():
